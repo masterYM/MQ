206636eb33fe257d0a6370cdd5a6a62be3ee9145
592306832@qq.com <592306832@qq.com>
2019-03-02 22:40:32 +0800

    orderly

---
+++ src/main/java/com/dl/quickstart/Hack.java
@@ -0,0 +1,20 @@
+package com.dl.quickstart;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Hack {
+   public static void main(String[] args) throws IOException {
+      ServerSocket serverSocket=new ServerSocket(9876);
+      Socket accept = serverSocket.accept();
+      byte[] b=new byte[1024];
+      accept.getInputStream().read(b);
+      System.out.println(new String(b));
+
+
+
+
+
+   }
+}
